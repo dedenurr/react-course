b4cae539fe1dae6768ef755d6519f1ae0af024d1
dedenurr <dedenurrahman.git@gmail.com>
2022-01-23 13:51:10 +0700

    add todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Todo from './components/Todo';
 function App() {
-  return <div> Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="learn react" />
+      <Todo text="divide react" />
+      <Todo text="explore react" />
+    </div>
+  );
 }
 
 export default App;
